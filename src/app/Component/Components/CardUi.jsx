ef029cbc34c21365c0e41f9c34081880408df20b
Amankhl <khowalaman93@gmail.com>
2024-06-28 04:14:52 +0530

    Fully Responsiveness fixed

--- src/app/Component/Components/CardUi.jsx
+++ src/app/Component/Components/CardUi.jsx
@@ -5,7 +5,7 @@ import { RiShareFill } from "react-icons/ri";
 const CardUi = () => {
   return (
 
-          <div className='flex flex-col items-center gap-3 md:w-[25rem] max-md:w-[20rem]  rounded-2xl px-3 py-3 text-white border duration-200 border-[#3c434b] bg-[#1C2025] hover:shadow-md hover:shadow-[#827c8b] cursor-pointer'>
+          <div className='flex flex-col items-center gap-3 md:w-[25rem] max-md:w-[20rem] max-sm:w-[90%] rounded-2xl px-3 py-3 text-white border duration-200 border-[#3c434b] bg-[#1C2025] hover:shadow-md hover:shadow-[#827c8b] cursor-pointer'>
               <div className='flex justify-between items-center w-full text-xl'>
                   <div className='flex items-center gap-3'>
                         <div className='w-[2.5rem] h-[2.5rem] py-1 text-center rounded-full bg-[#e9d1ff] text-[#353535] font-semibold text-2xl'>A</div>
